c70f2c6160a30536da1905112eade1c093cf40de
CosmicLatte009 <ohhm009@gmail.com>
2023-10-23 17:50:31 +0900

    Chore: Flutter create 및 app_color 파일 추가

---
+++ lib/util/app_color.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  //neutral
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF030303);
+  static const Color black90 = Color(0xFF1A1A1A);
+  static const Color black80 = Color(0xFF333333);
+  static const Color black70 = Color(0xFF4D4D4D);
+  static const Color black60 = Color(0xFF4D4D4D);
+  static const Color black50 = Color(0xFF808080);
+  static const Color black40 = Color(0xFF999999);
+  static const Color black30 = Color(0xFFB3B3B3);
+  static const Color black20 = Color(0xFFCCCCCC);
+  static const Color black10 = Color(0xFFE6E6E6);
+  static const Color black05 = Color(0xFFF3F3F3);
+
+  //primary
+  static const Color primary = Color(0xFF0059FF);
+  static const Color primary90 = Color(0xFF196AFF);
+  static const Color primary80 = Color(0xFF337AFF);
+  static const Color primary70 = Color(0xFF4C8BFF);
+  static const Color primary60 = Color(0xFF669BFF);
+  static const Color primary50 = Color(0xFF7FACFF);
+  static const Color primary40 = Color(0xFF99BDFF);
+  static const Color primary30 = Color(0xFFB2CDFF);
+  static const Color primary20 = Color(0xFFCCDEFF);
+  static const Color primary10 = Color(0xFFE5EEFF);
+  static const Color primary05 = Color(0xFFF5F8FF);
+
+  //stroke & line
+  static const Color strokeBlue = Color(0xFF99BDFF);
+  static const Color stroke10 = Color(0xFFE6E6E6);
+  static const Color stroke05 = Color(0xFFF3F3F3);
+
+  //background
+  static const Color back100 = Color(0xFF666666);
+  static final Color back60 = const Color(0xFF666666).withOpacity(0.6);
+  static const Color back05 = Color(0xFFF3F3F3);
+  static const Color backBlue = Color(0xFFF5F8FF);
+
+  //system
+  static const Color warning = Color(0xFFFF0000);
+  static const Color success = Color(0xFF07A320);
+}

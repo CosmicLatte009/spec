69704b2651f2c1b340511cf19634c159e6485b7e
Ko-Eunseo <koyk0408@naver.com>
2023-10-24 22:53:21 +0900

    Feat: popup sketch(in progress) #1

---
+++ lib/view/widget/popup/filter_items.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:spec/util/app_text_style.dart';
+import 'package:spec/view/widget/button/button_xsmall.dart';
+
+class FilterItems extends StatelessWidget {
+  const FilterItems({
+    super.key,
+    required this.list,
+    required this.label,
+  });
+  final List<String> list;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: AppTextStyles.body14M(),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Row(
+          children: list
+              .map((item) => Padding(
+                    padding: const EdgeInsets.only(right: 8),
+                    child: ButtonXsmall(
+                      controller: () {},
+                      text: item,
+                      width: 57,
+                    ),
+                  ))
+              .toList(),
+        ),
+        const SizedBox(
+          height: 32,
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/view/widget/popup/profile_popup.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:spec/util/app_color.dart';
+import 'package:spec/util/app_text_style.dart';
+import 'package:spec/view/widget/button/button_xsmall.dart';
+import 'package:spec/view/widget/popup/popup.dart';
+
+class ProfilePopup extends StatelessWidget {
+  const ProfilePopup({
+    super.key,
+    required this.name,
+    required this.role,
+    required this.position,
+  });
+  final String name;
+  final String role;
+  final String position;
+
+  @override
+  Widget build(BuildContext context) {
+    return Popup(
+      label: '프로필',
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                vertical: 34,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Stack(
+                    //아바타 완성되면 수정하기
+                    clipBehavior: Clip.none,
+                    children: [
+                      const CircleAvatar(
+                        backgroundColor: AppColor.primary05,
+                        radius: 30,
+                      ),
+                      Positioned(
+                        right: 0,
+                        left: 0,
+                        bottom: -10,
+                        child: Text(
+                          position,
+                          style: AppTextStyles.body12R(),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(
+                    width: 16,
+                  ),
+                  Text(
+                    name,
+                    style: AppTextStyles.body14B(),
+                  ),
+                  const SizedBox(
+                    width: 2.5,
+                  ),
+                  ButtonXsmall(
+                    //버튼 완성되면 컬러 바꾸기
+                    controller: () {},
+                    text: role,
+                    width: 47,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

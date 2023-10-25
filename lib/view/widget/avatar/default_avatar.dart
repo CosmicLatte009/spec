6f478615be42079dc7cc3e9878fa31fc5ee51254
Ko-Eunseo <koyk0408@naver.com>
2023-10-25 19:29:02 +0900

    Design: 아바타 헤어 위치 조정 가능하도록 수정

--- lib/view/widget/avatar/default_avatar.dart
+++ lib/view/widget/avatar/default_avatar.dart
@@ -2,7 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
 class DefaultAvatar extends StatelessWidget {
-  const DefaultAvatar({super.key});
+  const DefaultAvatar({
+    super.key,
+    this.width,
+    this.hairPosition,
+  });
+  final double? width;
+  final double? hairPosition;
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +17,13 @@ class DefaultAvatar extends StatelessWidget {
       children: [
         SvgPicture.asset(
           'assets/avatar/face/Face_smile.svg',
-          width: 40,
+          width: width ?? 40,
         ),
         Positioned(
-          top: -6,
+          top: hairPosition ?? -7,
           child: SvgPicture.asset(
             'assets/avatar/hair/Hair_short1_test.svg',
-            width: 40,
+            width: width ?? 40,
           ),
         ),
       ],

ad632bc65e42680d6b7cc5cc97ca44be1aad928f
Ko-Eunseo <koyk0408@naver.com>
2023-10-27 18:34:12 +0900

    Design: 회원가입, 성공페이지 UI 구현

---
+++ lib/view/page/signup_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spec/util/app_color.dart';
+import 'package:spec/util/app_text_style.dart';
+import 'package:spec/view/page/signup_success_page.dart';
+import 'package:spec/view/widget/button/custom_button.dart';
+import 'package:spec/view/widget/custom_input.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final _signup_formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          '회원가입하기',
+          style: AppTextStyles.body18B(
+            color: AppColor.black,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const SizedBox(height: 24),
+            Text(
+              '*표시는 필수입력항목입니다.',
+              textAlign: TextAlign.right,
+              style: AppTextStyles.body8R(
+                color: AppColor.warning,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Form(
+                  key: _signup_formKey,
+                  child: const Column(
+                    children: [
+                      CustomInput(label: '이름', hint: '이름을 입력해주세요.'),
+                      SizedBox(height: 8),
+                      CustomInput(label: '이메일', hint: '이메일을 입력해주세요.'),
+                      SizedBox(height: 8),
+                      CustomInput(
+                        label: '비밀번호',
+                        hint: '비밀번호를 입력해주세요.',
+                        type: InputType.password,
+                      ),
+                      SizedBox(height: 8),
+                      CustomInput(label: '휴대전화', hint: '휴대폰 번호를 입력해주세요.'),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            CustomButton(
+              height: 56,
+              text: '회원가입하기',
+              onTap: () {
+                Get.to(const SignupSuccessPage());
+              },
+            ),
+            const SizedBox(height: 114),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/page/signup_success_page.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:spec/util/app_color.dart';
+import 'package:spec/util/app_text_style.dart';
+import 'package:spec/view/widget/button/custom_button.dart';
+
+class SignupSuccessPage extends StatelessWidget {
+  const SignupSuccessPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images/Graphic3.png'),
+                  const SizedBox(height: 8),
+                  Text(
+                    '환영합니다!',
+                    style: AppTextStyles.header24(),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'SPACE와 함께해봐요!',
+                    style: AppTextStyles.body16R(
+                      color: AppColor.black40,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            CustomButton(
+              text: '프로필 작성하기',
+              height: 56,
+            ),
+            const SizedBox(height: 114),
+          ],
+        ),
+      ),
+    );
+  }
+}

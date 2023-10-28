9ff6c6c1bcb39ffbd5747887fae1524224abaf77
CosmicLatte009 <ohhm009@gmail.com>
2023-10-29 01:36:13 +0900

    Design: ForgotPasswordPage, ChangePasswordPage UI 구현

---
+++ lib/view/page/change_password_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:spec/view/widget/button/custom_button.dart';
+import 'package:spec/view/widget/navigation/nav_menu.dart';
+import 'package:spec/view/widget/navigation/top.dart';
+import 'package:spec/view/widget/custom_input.dart';
+
+class ChangePasswordPage extends StatelessWidget {
+  const ChangePasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      body: Column(
+        children: [
+          const SizedBox(height: 24),
+          const NavMenu(
+            title: '비밀번호 변경',
+            titleDirection: TitleDirection.center,
+          ),
+          const SizedBox(height: 32),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              child: Form(
+                child: Column(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          CustomInput(
+                            controller: TextEditingController(),
+                            label: '현재 비밀번호',
+                            hint: '비밀번호',
+                            type: InputType.password,
+                          ),
+                          const SizedBox(height: 8),
+                          CustomInput(
+                            controller: TextEditingController(),
+                            label: '새 비밀번호',
+                            hint: '비밀번호',
+                            type: InputType.password,
+                          ),
+                        ],
+                      ),
+                    ),
+                    CustomButton(
+                      // onTap: () {},
+                      text: '변경하기',
+                      type: ButtonType.main,
+                      height: 56,
+                      disabled: true,
+                    ),
+                    const SizedBox(height: 113),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

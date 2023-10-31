ec32335ecfd1abf5f98ad143c9549ad467ccf1d3
Ko-Eunseo <koyk0408@naver.com>
2023-10-31 13:18:11 +0900

    Feat: badge 타입 추가. author에서 에러 발생으로 인해 코멘트 처리

--- lib/model/author.dart
+++ lib/model/author.dart
@@ -1,7 +1,10 @@
+import 'package:spec/model/badge.dart';
+
 class Author {
   String id;
   String? avatar;
   String nickname;
+  // Badge badge;
   String role;
   String position;
   int temperature;
@@ -10,6 +13,7 @@ class Author {
     required this.id,
     required this.avatar,
     required this.nickname,
+    // this.badge,
     required this.role,
     required this.position,
     required this.temperature,
@@ -23,6 +27,7 @@ class Author {
       role: map['role'],
       position: map['position'],
       temperature: map['temperature'],
+      // badge: Badge.fromMap(map['badge']),
     );
   }
 }

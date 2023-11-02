d0f09c189845c931c3ee24c48c7a53f241c71eb1
CosmicLatte009 <ohhm009@gmail.com>
2023-11-02 13:54:27 +0900

    Feat: AllTalkPage 업데이트

---
+++ lib/view/page/talk/all_talk_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:spec/controller/talk/all_talk_controller.dart';
+import 'package:spec/util/app_page_routes.dart';
+import 'package:spec/view/widget/talk/talk_bubble_builder.dart';
+import 'package:spec/view/widget/custom_input.dart';
+import 'package:spec/view/widget/navigation/bottomnavigationbar.dart';
+import 'package:spec/view/widget/navigation/nav_menu.dart';
+import 'package:spec/view/widget/navigation/top.dart';
+import '../../../util/app_color.dart';
+import '../../widget/button/button_circle.dart';
+
+class AllTalkPage extends GetView<AllTalkController> {
+  const AllTalkPage({super.key});
+  static const route = '/talk/all';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      floatingActionButton: CircleButton(
+        svg: 'assets/icons/svgs/Community_white.svg',
+        iconWidth: 26,
+        buttonWidth: 50,
+        backColor: AppColor.primary80,
+        onTap: () {
+          print('호잇');
+        },
+      ),
+      bottomNavigationBar: CustomBottomNavigationBar(
+        currentIndex: 1,
+        onTap: (val) {
+          print(val);
+        },
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 10.0),
+              child: CustomInput(
+                type: InputType.search,
+              ),
+            ),
+            NavMenu(
+              title: '톡톡톡',
+              titleDirection: TitleDirection.center,
+              // onButtonPressed: () => Get.toNamed(AppPagesRoutes.mainTalk),
+            ),
+            const SizedBox(height: 16),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Obx(() {
+                if (controller.isLoading.value) {
+                  return const CircularProgressIndicator();
+                } else {
+                  return TalkBubbleBuilder(talkDataList: controller.allTalks);
+                }
+              }),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

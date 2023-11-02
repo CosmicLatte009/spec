bf164c5c3478831feb25a710600d52a77cf1ada6
seeunson <godjimin010628@gmail.com>
2023-11-02 14:56:17 +0900

    Fix: Profile 모델 충돌 해결

--- lib/model/profile.dart
+++ lib/model/profile.dart
@@ -1,61 +1,31 @@
+import 'badge.dart';
+
 class Profile {
-  String id;
-  String nickname;
-  String bio;
-  String? avatar;
-  String position;
-  String createdAt;
-  String updatedAt;
-  String? urlGithub;
-  String? urlBehance;
-  String? urlBlog;
-  String? urlWeb;
-  String? urlEtc;
-  String? urlPortfolio;
-  String role;
-  String? badgeId;
-  String accountId;
-  int temperature;
+  final String id;
+  final String? avatar;
+  final String nickname;
+  final Badge badge;
+  final String role;
+  final String position;
+  final int temperature;
 
   Profile({
     required this.id,
-    required this.nickname,
-    required this.bio,
     this.avatar,
-    required this.position,
-    required this.createdAt,
-    required this.updatedAt,
-    this.urlGithub,
-    this.urlBehance,
-    this.urlBlog,
-    this.urlWeb,
-    this.urlEtc,
-    this.urlPortfolio,
+    required this.nickname,
+    required this.badge,
     required this.role,
-    this.badgeId,
-    required this.accountId,
+    required this.position,
     required this.temperature,
   });
 
-  factory Profile.fromMap(Map<String, dynamic> map) {
-    return Profile(
-      id: map['id'],
-      nickname: map['nickname'],
-      bio: map['bio'],
-      avatar: map['avatar'],
-      position: map['position'],
-      createdAt: map['createdAt'],
-      updatedAt: map['updatedAt'],
-      urlGithub: map['urlGithub'],
-      urlBehance: map['urlBehance'],
-      urlBlog: map['urlBlog'],
-      urlWeb: map['urlWeb'],
-      urlEtc: map['urlEtc'],
-      urlPortfolio: map['urlPortfolio'],
-      role: map['role'],
-      badgeId: map['badgeId'],
-      accountId: map['accountId'],
-      temperature: map['temperature'],
-    );
-  }
+  factory Profile.fromMap(Map<String, dynamic> map) => Profile(
+        id: map['id'],
+        avatar: map['avatar'],
+        nickname: map['nickname'],
+        badge: Badge.fromMap(map['badge']),
+        role: map['role'],
+        position: map['position'],
+        temperature: map['temperature'],
+      );
 }

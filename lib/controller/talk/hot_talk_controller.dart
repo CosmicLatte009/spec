e76d755a39ca990ac49d176041d6a25be0562d27
CosmicLatte009 <ohhm009@gmail.com>
2023-11-04 05:54:46 +0900

    Feat: MainTalkController에서 mainTalksLoading으로 로딩상태 확인

--- lib/controller/talk/hot_talk_controller.dart
+++ lib/controller/talk/hot_talk_controller.dart
@@ -21,18 +21,16 @@ class HotTalkController extends GetxController {
         var resData = List<Map<String, dynamic>>.from(res.data["data"]);
         var talkList = resData.map(Talk.fromMap).toList();
         _hotTalks.assignAll(talkList);
+        // print(hotTalks);
         isLoading.value = false;
-        print(_hotTalks);
       } else {
         isLoading.value = true;
         print(res.data["message"]);
       }
     } on DioException catch (e) {
       isLoading.value = true;
-      print('핫한 톡 가져오기 실패여');
       print(e.toString());
     }
-    // print('요청을 보내고 있긴 하니');
   }
 
   @override
@@ -40,6 +38,5 @@ class HotTalkController extends GetxController {
     super.onInit();
     dio.options.baseUrl = baseUrl;
     await getHotTalks();
-    print('핫톡 컨트롤러 onInit은 되긴 되니');
   }
 }

b60de6b64d5cc9aa3a0efc66c830bda2a1352216
Ko-Eunseo <koyk0408@naver.com>
2023-11-03 18:47:53 +0900

    Feat: 모각 수정 기능 추가 #27

--- lib/controller/mogak/create_mogak_controller.dart
+++ lib/controller/mogak/create_mogak_controller.dart
@@ -7,7 +7,6 @@ class CreateMogakController extends GetxController {
   var authController = Get.find<AuthController>();
 
   Dio dio = Dio();
-  String path = '/api/mogak';
   List<String> visiblityStatus = ["HIDDEN", "OPEN", "CLOSE"];
   final RxInt _selectedIndex = RxInt(0);
 
@@ -26,6 +25,8 @@ class CreateMogakController extends GetxController {
   }
 
   submitAction() async {
+    String path = '/api/mogak';
+
     String content = titleController.text; //5자 이상, 50자 이하
     String title = contentsController.text; // 10자 이상, 500자 이하
     int maxMember = int.parse(maxNumberController.text); // 1명 이상 100명 이하
@@ -34,6 +35,38 @@ class CreateMogakController extends GetxController {
 
     try {
       var res = await dio.post(path, data: {
+        'content': content,
+        'title': title,
+        'maxMember': maxMember,
+        'hashtag': hashtag,
+        'visiblityStatus': visiblityState,
+      });
+
+      print(res.data);
+      if (res.statusCode == 200) {
+        if (res.data["status"]) {
+          //@todo submitAction이 발생하면 어디로 리다이렉트 할 것인가?
+          print(res.data["data"]);
+        } else {
+          print(res.data["message"]);
+        }
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  updateMogak({required String mogakId}) async {
+    String path = "/api/mogak/$mogakId";
+
+    String content = titleController.text; //5자 이상, 50자 이하
+    String title = contentsController.text; // 10자 이상, 500자 이하
+    int maxMember = int.parse(maxNumberController.text); // 1명 이상 100명 이하
+    String hashtag = tagController.text; // 디폴트: 빈 문자열
+    String visiblityState = _visiblityState; // 값 입력하지 않았을 경우 OPEN으로 default
+
+    try {
+      var res = await dio.put(path, data: {
         'content': content,
         'title': title,
         'maxMember': maxMember,

188a12951c9e3c405f45301f1d094c823a93283e
CosmicLatte009 <ohhm009@gmail.com>
2023-11-04 23:06:47 +0900

    Fix: Talk 모델에 catchUpId, mogakId 속성 추가

--- lib/model/talk.dart
+++ lib/model/talk.dart
@@ -5,27 +5,31 @@ class Talk {
   String id;
   String authorId;
   String? parentId;
+  String? mogakId;
+  String? catchUpId;
   String content;
   DateTime createdAt;
   DateTime updatedAt;
   bool isDeleted;
   Author author;
-  int? childrenLength;
   List<Author>? upProfiles;
   int temperature;
+  int? childrenLength;
   List<Talk>? children;
   Talk({
     required this.id,
     required this.authorId,
     this.parentId,
+    this.mogakId,
+    this.catchUpId,
     required this.content,
     required this.createdAt,
     required this.updatedAt,
     required this.isDeleted,
     required this.author,
-    this.childrenLength,
     this.upProfiles,
     required this.temperature,
+    this.childrenLength,
     this.children,
   });
 
@@ -34,13 +38,15 @@ class Talk {
       id: map['id'] ?? '',
       authorId: map['authorId'],
       parentId: map['parentId'],
+      mogakId: map['mogakId'],
+      catchUpId: map['catchUpId'],
       content: map['content'],
       createdAt: DateTime.parse(map['createdAt']),
       updatedAt: DateTime.parse(map['updatedAt']),
       isDeleted: map['deletedAt'] !=
           null, // JSON에는 deletedAt이 존재하므로 이를 바탕으로 isDeleted 상태를 결정합니다.
       author: Author.fromMap(map['author'] as Map<String, dynamic>),
-      childrenLength: map['childrenLength'] ?? 0,
+
       upProfiles: map['upProfiles'] != null
           ? List<Author>.from(
               (map['upProfiles'] as List).map(
@@ -49,6 +55,7 @@ class Talk {
             )
           : null,
       temperature: map['temperature'] ?? 0,
+      childrenLength: map['childrenLength'] ?? 0,
       children: map['children'] != null
           ? List<Talk>.from((map['children'] as List)
               .map((x) => Talk.fromMap(x as Map<String, dynamic>)))
@@ -58,6 +65,6 @@ class Talk {
 
   @override
   String toString() {
-    return 'Talk(id: $id, authorId: $authorId, parentId: $parentId, content: $content, createdAt: $createdAt, updatedAt: $updatedAt, isDeleted: $isDeleted, author: $author, childrenLength: $childrenLength, upProfiles: $upProfiles, temperature: $temperature, children: $children)';
+    return 'Talk(id: $id, authorId: $authorId, parentId: $parentId, mogakId: $mogakId, catchUpId: $catchUpId, content: $content, createdAt: $createdAt, updatedAt: $updatedAt, isDeleted: $isDeleted, author: $author,  upProfiles: $upProfiles, temperature: $temperature, childrenLength: $childrenLength, children: $children)';
   }
 }

bb6e6fd26be24a3fff68b9a35478db2b8afed114
Ko-Eunseo <koyk0408@naver.com>
2023-11-08 17:41:45 +0900

    Refactor: 레이아웃 수정

--- lib/view/page/mogak/all_mogak_page.dart
+++ lib/view/page/mogak/all_mogak_page.dart
@@ -13,83 +13,88 @@ import 'package:spec/view/widget/navigation/top.dart';
 class AllMogakPage extends GetView<AllMogakController> {
   const AllMogakPage({super.key});
   static const route = '/mogak/all';
+  static const title = '모든 모각코';
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const SizedBox(
-              height: 16,
-            ),
-            CustomInput(
+      body: Column(
+        children: [
+          const SizedBox(height: 16),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: CustomInput(
               type: InputType.search,
               controller: controller.searchController.searchTextController,
               onSubmit: (value) {
                 controller.controller.getAllMogak(query: value);
               },
             ),
-            const SizedBox(height: 6),
-            Expanded(
-              child: ListView(
-                children: [
-                  //이전페이지 네비게이션
-                  const NavMenu(
-                    title: '모든 모각코',
-                    titleDirection: TitleDirection.center,
-                  ),
-                  // FilterButton
-                  OrderbyButton(
-                    onTap: () {
-                      controller.filterController.toggleOrderBy();
-                      controller.controller.getAllMogak();
-                    },
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  //모각코 카드
-                  Expanded(
-                    child: Obx(
-                      () => ListView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: controller.allMogak!.length,
-                        itemBuilder: (context, index) {
-                          return Column(
-                            children: [
-                              MogakCard(
-                                mogak: controller.allMogak![index],
-                                mogakState: controller.mogakState(
-                                  controller.allMogak![index].visiblityStatus,
-                                ),
-                                isUped: controller
-                                    .isUped(controller.allMogak![index].id),
-                                controller: controller.toggleLike,
-                              ),
-                              const SizedBox(height: 8),
-                              StackAvatars(
-                                commentLength: controller
-                                    .allMogak![index].appliedProfiles.length,
-                                upLength: controller
-                                    .allMogak![index].upProfiles.length,
-                              ),
-                              const SizedBox(height: 16),
-                            ],
-                          );
+          ),
+          const SizedBox(height: 6),
+          Expanded(
+            child: ListView(
+              children: [
+                //이전페이지 네비게이션
+                const NavMenu(
+                  title: title,
+                  titleDirection: TitleDirection.center,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: Column(
+                    children: [
+                      // FilterButton
+                      OrderbyButton(
+                        onTap: () {
+                          controller.filterController.toggleOrderBy();
+                          controller.controller.getAllMogak();
                         },
                       ),
-                    ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      //모각코 카드
+                      Obx(
+                        () => ListView.builder(
+                          physics: const NeverScrollableScrollPhysics(),
+                          shrinkWrap: true,
+                          itemCount: controller.allMogak!.length,
+                          itemBuilder: (context, index) {
+                            return Column(
+                              children: [
+                                MogakCard(
+                                  mogak: controller.allMogak![index],
+                                  mogakState: controller.mogakState(
+                                    controller.allMogak![index].visiblityStatus,
+                                  ),
+                                  isUped: controller
+                                      .isUped(controller.allMogak![index].id),
+                                  controller: controller.toggleLike,
+                                  title: title,
+                                ),
+                                const SizedBox(height: 8),
+                                StackAvatars(
+                                  commentLength: controller
+                                          .allMogak![index].childrenLength ??
+                                      0,
+                                  upLength: controller
+                                      .allMogak![index].upProfiles.length,
+                                ),
+                                const SizedBox(height: 16),
+                              ],
+                            );
+                          },
+                        ),
+                      ),
+                    ],
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
       floatingActionButton: CustomFloatingActionButton(
         onPressed: () {
